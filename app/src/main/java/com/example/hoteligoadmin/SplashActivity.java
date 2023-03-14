a5d81e6e4607292308cc81dca125c90a72f63176
prathamesh <prathameshkalaskar14@gmail.com>
2023-03-14 20:51:55 +0530

    Final commit of hoteligo admin application

---
+++ app/src/main/java/com/example/hoteligoadmin/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.hoteligoadmin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLSH_TIME_OUT = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLSH_TIME_OUT);
+    }
+}
